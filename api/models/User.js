98c7b909ecfcaecd4f17bb3e9cad7de80a04696a
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2022-12-20 16:38:16 -0800

    Fix typos

--- api/models/User.js
+++ api/models/User.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema(
   {
-    userName: {
+    username: {
       type: String,
       required: true,
       unique: true,
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -3,6 +3,6 @@ import { register } from '../controllers/auth.js';
 
 const router = express.Router();
 
-router.get('/register', register);
+router.post('/register', register);
 
 export default router;
