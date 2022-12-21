0040a956e5d4ded0bf06666ddae86a978f00d7b5
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2022-12-20 23:24:05 -0800

    Add auth controller and hash passwords

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,11 +1,15 @@
 import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
 
 export const register = async (req, res, next) => {
   try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hash,
     });
 
     await newUser.save();
