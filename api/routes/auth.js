2e7d7cd707b7373a9f1352234cdff8a9ada0616e
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2022-12-20 16:10:51 -0800

    Add controllers and update routes

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,12 +1,8 @@
 import express from 'express';
+import { register } from '../controllers/auth.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Hello, this is auth endpoint');
-});
-router.get('/register', (req, res) => {
-  res.send('Hello, this is auth register endpoint');
-});
+router.get('/register', register);
 
 export default router;
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,33 +1,27 @@
 import express from 'express';
-import Hotel from '../models/Hotel.js';
+import {
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotels,
+  updateHotel,
+} from '../controllers/hotel.js';
 
 const router = express.Router();
 
 //CREATE
-router.post('/', async (req, res) => {
-  const newHotel = new Hotel(req.body);
-
-  try {
-    const savedHotel = await newHotel.save();
-    res.status(200).json(savedHotel);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.post('/', createHotel);
 
 //UPDATE
-router.put('/:id', async (req, res) => {
-  try {
-    const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {
-      $set: req.body,
-    });
-    res.status(200).json(updatedHotel);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.put('/:id', updateHotel);
+
 //DELETE
+router.delete('/:id', deleteHotel);
+
 //GET
+router.get('/:id', getHotel);
+
 //GET ALL
+router.get('/', getHotels);
 
 export default router;
