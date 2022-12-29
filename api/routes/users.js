a9a2483a403d5c82e3221a38a8549b64f5da3845
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2022-12-28 23:18:15 -0800

    Create user controller

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,23 @@
 import express from 'express';
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from '../controllers/user.js';
 
 const router = express.Router();
+
+//UPDATE
+router.put('/:id', updateUser);
+
+//DELETE
+router.delete('/:id', deleteUser);
+
+//GET
+router.get('/:id', getUser);
+
+//GET ALL
+router.get('/', getUsers);
 
 export default router;
