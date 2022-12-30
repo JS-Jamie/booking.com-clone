5f97bd99a54d36d8794d30fadd798c5a791ca935
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2022-12-30 15:42:16 -0800

    Create a custom hook for featured cities and property list

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import {
   countByCity,
+  countByType,
   createHotel,
   deleteHotel,
   getHotel,
@@ -26,6 +27,6 @@ router.get('/find/:id', getHotel);
 //GET ALL
 router.get('/', getHotels);
 router.get('/countByCity', countByCity);
-router.get('/countByType', getHotels);
+router.get('/countByType', countByType);
 
 export default router;
