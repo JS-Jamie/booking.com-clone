c67fc63e301eed9ea30ca3e6307fb8ddc1b06334
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2023-01-10 00:00:12 -0800

    update login

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
 import './login.css';
 import axios from 'axios';
+import Navbar from '../../components/navbar/Navbar';
 
 const Login = () => {
   const [credentials, setCredentials] = useState({
@@ -31,25 +32,32 @@ const Login = () => {
   };
 
   return (
-    <div className='login'>
-      <input
-        type='text'
-        placeholder='username'
-        id='username'
-        onChange={handleChange}
-        className='lInput'
-      />
-      <input
-        type='password'
-        placeholder='password'
-        id='password'
-        onChange={handleChange}
-        className='lInput'
-      />
-      <button disabled={loading} onClick={handleClick} className='lButton'>
-        Login
-      </button>
-      {error && <span>{error.message}</span>}
+    <div>
+      <Navbar />
+
+      <div className='login'>
+        <div className='emailSection'>
+          <h1>Sign in or create an account</h1>
+          <h3>Email address</h3>
+          <input
+            type='text'
+            id='username'
+            onChange={handleChange}
+            className='lInput'
+          />
+          <button disabled={loading} onClick={handleClick} className='lButton'>
+            Continue with email
+          </button>
+          {error && <span>{error.message}</span>}
+        </div>
+        <div className='accessPanel'>
+          <div className='divider'>
+            <hr></hr>
+            <span className='dividerText'>or use one of these options</span>
+            <hr></hr>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
